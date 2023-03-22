ed1012afd259c03fc199737b3ded9a68f76e9345
Victoria Pham <victoriapham52@gmail.com>
2023-03-22 16:15:02 -0700

    Static Exercise commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("What would you like to convert to Celcius?");
+            var farenheitInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{farenheitInput} degrees Fahrenheit is {TempConverter.FahrenheitToCelsius(farenheitInput)} degrees Celcius.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("What would you like to convert to Fahrenheit?");
+            var celciustInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{celciustInput} degrees Celcius is {TempConverter.CelsiusToFahrenheit(celciustInput)} degrees Fahrenheit.");
+
         }
+            
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+        public static double FahrenheitToCelsius(double fNum)
+        {
+            var fahrenheit = (fNum - 32) * (5d / 9d);
+
+            return fahrenheit;
+
+
+        }
+
+        public static  double CelsiusToFahrenheit(double cNum)
+        {
+            var celcius = cNum * (9d/5d) + 32;
+            return celcius; 
+
+        }
+
+
+    }
+}
+
